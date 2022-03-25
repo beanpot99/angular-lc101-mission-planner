170c80a78573670161e4febbafa631a72df2e15a
Grace Thompson <Grace.thompson17@aol.com>
2022-03-24 20:05:55 -0500

    everything but update crew members names

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-
+  newCrewMem:string='Add a crew member!';
+  firstMissionCheck:boolean=false;
+  memberBeingEdited:object=null;
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
@@ -16,6 +18,20 @@ export class CrewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  addCrew(memberName:string,isFirst:boolean){
+    this.crew.push({name:memberName,firstMission: isFirst})
+  }
+  removeMember(crewMem: object){
+    let index=this.crew.indexOf(crewMem);
+    this.crew.splice(index,1);
+  }
+  edit(crewMem:object){
+    this.memberBeingEdited =crewMem;
+  }
+  save(name:string, crewMem:object){
+    crewMem['name']=updatedName;
+    this.memberBeingEdited=null;
   }
 
 }
